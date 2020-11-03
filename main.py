f1ccd45fd4e9db5ad939e6caf5992e9b2604b0fa
Gareth Tribello <gareth.tribello@gmail.com>
2020-11-03 18:54:22 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import sympy as sy
+
+# I have defined the symbols for you to use in your expressions
+x, y, k = sy.symbols('x y k')
+# You need to add a sympy command after the comma here to calculate the derivative of 
+# the pdf with respect to x
+dfdx =
+print("The partial derivative of the pdf with respect to x is",dfdx )  
+# You need to add a sympy command after the comma here to calculate the derivative of 
+# the pdf with respect to y
+dfdy = 
+print("The partial derivative of the pdf with respect to y is",dfdy ) 
+# You need to add a sympy command after the comma here to calculate the second partial derivative of 
+# the pdf with respect to x then y
+d2dxdy = 
+print("d^2 f / dxdy =", d2dxdy )
+# You need to add a sympy command after the comma here to calculate the second partial derivative of 
+# the pdf with respect to y then x
+d2dydx = 
+print("d^2 f / dydx =", d2dydx )
